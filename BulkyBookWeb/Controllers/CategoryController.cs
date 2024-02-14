5204b807e55118bf5091adac003f92c37ea69d40
BinuMDX <binarigpv@gmail.com>
2024-02-14 13:31:08 +0530

    Add Create Operation

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -17,5 +17,12 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //Get 
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
     }
 }
